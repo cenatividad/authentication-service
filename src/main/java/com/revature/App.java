11a66bdcdc356b1428a6de6732e0224a46a439c7
NoahThacker31 <31545990+NoahThacker31@users.noreply.github.com>
2019-03-25 11:21:40 -0400

    Swagger UI

--- src/main/java/com/revature/App.java
+++ src/main/java/com/revature/App.java
@@ -4,10 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 /**
  * The Class App.
  * @author 1811-Java-Nick 12/27/18
  */
+@EnableSwagger2
 @SpringBootApplication
 @EnableFeignClients
 public class App {
